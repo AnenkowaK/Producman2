e076cc57db6cb6b7da4566448b7e1a2710997f69
Ekaterina Smetanina <AnenkowaK@mail.ru>
2021-09-30 12:18:19 +0300

    initial commit

---
+++ src/test/java/ru/netology/domain/ProductManagerTest.java
@@ -0,0 +1,43 @@
+package ru.netology.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import ru.netology.manager.ProductManager;
+import ru.netology.domain.Book;
+import ru.netology.domain.Smartphone;
+import ru.netology.domain.Product;
+import ru.netology.repository.ProductRepository;
+
+public class ProductManagerTest {
+
+
+
+
+
+    @Test
+    public void shouldSearchProductBook() {
+        Book coreJava = new Book(1, "qqq", 1, "www");
+        Smartphone samsung=new Smartphone(2, "eee", 2, "rrrr");
+        ProductRepository repository = new ProductRepository();
+        ProductManager manager=new ProductManager(repository);
+        manager.add(coreJava);
+        manager.add(samsung);
+        Product[] expected = new Product[]{coreJava};
+        Product[] actual = manager.searchBy("eee");
+        assertArrayEquals(expected,actual);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
