92c8899ad3b36c0878553e4002b00447eae93a6a
Ekaterina Smetanina <AnenkowaK@mail.ru>
2021-10-20 17:12:04 +0300

    добавлены тесты

--- src/test/java/ru/netology/domain/ProductManagerTest.java
+++ src/test/java/ru/netology/domain/ProductManagerTest.java
@@ -16,9 +16,9 @@ import javax.swing.*;
 
 
 public class ProductManagerTest {
-    Book coreJava = new Book(1, "qqq", 1, "www");
-    Smartphone samsung = new Smartphone(2, "eee", 2, "rrrr");
-    TShirt polo = new TShirt(3, "ppp", 3, "lll", "xl");
+    Book coreJava = new Book(1, "aqqq", 1, "bww");
+    Smartphone samsung = new Smartphone(2, "aeee", 2, "brrrr");
+    TShirt polo = new TShirt(3, "appp", 3, "lllb", "xl");
 
     ProductRepository repository = new ProductRepository();
     ProductManager manager = new ProductManager(repository);
@@ -32,9 +32,58 @@ public class ProductManagerTest {
     }
 
     @Test
-    public void shouldSearchProductBook() {
+    public void shouldSearchProductBookName() {
         Product[] expected = new Product[]{coreJava};
         Product[] actual = manager.searchBy("qqq");
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldSearchProductBookAuthor() {
+        Product[] expected = new Product[]{coreJava};
+        Product[] actual = manager.searchBy("bww");
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldSearchProductSmartphoneName() {
+        Product[] expected = new Product[]{samsung};
+        Product[] actual = manager.searchBy("aeee");
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldSearchProductSmartphoneManufacturer() {
+        Product[] expected = new Product[]{samsung};
+        Product[] actual = manager.searchBy("brrrr");
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldSearchProductTShirtName() {
+        Product[] expected = new Product[]{polo};
+        Product[] actual = manager.searchBy("appp");
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldSearchProductTShirtColor() {
+        Product[] expected = new Product[]{polo};
+        Product[] actual = manager.searchBy("lllb");
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldSearchProductTShirtSize() {
+        Product[] expected = new Product[]{};
+        Product[] actual = manager.searchBy("xl");
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldSearchProductAllName() {
+        Product[] expected = new Product[]{coreJava, samsung, polo};
+        Product[] actual = manager.searchBy("a");
         assertArrayEquals(expected, actual);
     }
 }
